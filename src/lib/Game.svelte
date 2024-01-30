cf8ce00f0dfef20e0314939ded968add38ad36bd
Surya Madhavan <kmaddy9122@gmail.com>
2024-01-30 23:07:50 +0530

    Minor logic addition

--- src/lib/Game.svelte
+++ src/lib/Game.svelte
@@ -5,6 +5,7 @@
 	let oversSelected = false;
 
 	let overs = 0;
+	let wickets = 0;
     let currentOver = 1;
     /**
      * @type boolean
@@ -18,11 +19,27 @@
 		overs = a;
 	}
 
+
+	/**
+	 * @param {number} a
+	 */
+	function setWickets(a) {
+		wickets = a;
+	}
+
 	/**
 	 * reset the overs input
 	 */
-	function setCustom() {
+	function setCustomOvers() {
 		overs = 0;
+	}
+
+
+	/**
+	 * reset the overs input
+	 */
+	function setCustomWickets() {
+		wickets = 0;
 	}
 
     /**
@@ -37,12 +54,19 @@
 	<h2>Select Overs</h2>
 	<button class="base-button" on:click={() => setOvers(5)}>5</button>
 	<button class="base-button" on:click={() => setOvers(10)}>10</button>
-	<button class="base-button" on:click={setCustom}>Custom</button>
+	<button class="base-button" on:click={setCustomOvers}>Custom</button>
 	<input class="over-custom-input" placeholder="No. of Overs" bind:value={overs} type="number" />
-	{#if overs != 0}
+
+	<h2>Select Wickets</h2>
+	<button class="base-button" on:click={() => setOvers(5)}>5</button>
+	<button class="base-button" on:click={() => setOvers(10)}>10</button>
+	<button class="base-button" on:click={setCustomWickets}>Custom</button>
+	<input class="over-custom-input" placeholder="No. of Wickets" bind:value={wickets} type="number" />
+	{#if overs != 0 && wickets != 0}
 		<button class="base-button" on:click={saveSettings}>Save</button>
 	{/if}
     {:else}
     <h4>Overs: {overs}</h4>
+    <h4>Wickets: {wickets}</h4>
     <OverInput currentOver={currentOver} isOverCompleted={isOverCompleted} />
 {/if}
